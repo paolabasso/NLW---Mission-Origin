e6b2d22133100d5aa69ed5cd91e92c1ea9c18019
paolabasso <91506356+paolabasso@users.noreply.github.com>
2021-10-09 15:07:29 -0300

    Versão Final

--- main.js
+++ main.js
@@ -38,7 +38,10 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: { slidesPerView: 2, setWrapperSize: true }
+  }
 })
 
 /* ScrollReveal - mostrar elementos quando dar scroll na página */
@@ -53,8 +56,9 @@ scrollReveal.reveal(
   `#home .image, #home .text,
   #about .image, #about .text,
   #services header, #services .card,
-  #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links,
+  #testimonials header, 
+  #testimonials .testimonials #contact .text,
+  #contact .links,
   footer .brand, footer .social
   `,
   { interval: 100 }
@@ -70,8 +74,34 @@ function backToTopButton() {
   }
 }
 
+/* Ativar menu conforme a seção na página, quando eu clicar */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
 /* Funções que utilizam o scroll - When Scroll */
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
   backToTopButton()
+  activateMenuAtCurrentSection()
 })
